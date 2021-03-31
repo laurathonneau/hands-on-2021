b22226bbccd677401fcbf9997bbcb3f3f829187a
laurathonneau <80095555+laurathonneau@users.noreply.github.com>
2021-03-31 22:40:04 +0200

    Add files via upload

---
+++ scripts/download_images.sh
@@ -0,0 +1,8 @@
+cd ../data/raw
+curl -O https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Training_Images.zip
+curl -O https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Test_Images.zip
+curl -O https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Test_GT.zip
+
+unzip -q GTSRB_Final_Training_Images.zip
+unzip -q GTSRB_Final_Test_Images.zip
+unzip -q GTSRB_Final_Test_GT.zip
